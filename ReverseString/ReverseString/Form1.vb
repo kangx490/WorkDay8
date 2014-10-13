21201ba166f8031f72431bf9c7f77ffbc3eb1cb9
idsc3101 <idsc3101@gmail.com>
2014-10-13 15:15:35 -0500

    workday8

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -4,9 +4,13 @@ Public Class Form1
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim st As String = txtInput.Text
+        Dim result As String = ""
+        For i As Integer = st.Length - 1 To 0 Step -1
+            result &= st.Chars(i)
 
-        ' Please write your code here!
 
-        txtOutput.Text = "Not implemented yet!"
+        Next
+
+        txtOutput.Text = result
     End Sub
 End Class
